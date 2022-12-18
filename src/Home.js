01a64eace5b520df060fd6d4edfdf400a14bd4dd
HarryKing87 <babishh96@hotmail.com>
2022-12-18 15:36:32 +0200

    Navigation on mobile and desktop

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import Navigation from "./Navigation";
 import "./CSS/home.css";
+import "./CSS/home-banner.css";
 export default function Home() {
   return (
     <div className="home-component">
       <Navigation />
+      <div class="container">
+        <div class="neon">Pantry. </div>
+      </div>
+      <div className="home-content"></div>
     </div>
   );
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,29 +1,68 @@
 import "./CSS/navigation.css";
+import "./CSS/mobile-nav.css";
 export default function Navigation() {
   return (
-    <div className="mobile-nav">
-      <ul id="navigation-items-mobile">
-        <li>
-          <a href="/" style={{ fontWeight: "700" }}>
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/about" style={{ fontWeight: "700" }}>
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/projects" style={{ fontWeight: "700" }}>
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="/contact" style={{ fontWeight: "700" }}>
-            Contact Me
-          </a>
-        </li>
-      </ul>
+    <div className="nav-container">
+      <div className="mobile-nav">
+        <nav role="navigation">
+          <div id="menuToggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+              <a href="#">
+                <li>Home</li>
+              </a>
+              <a href="#">
+                <li>About</li>
+              </a>
+              <a href="#">
+                <li>Contact</li>
+              </a>
+              <a href="#">
+                <li>FAQ</li>
+              </a>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <div className="desktop-nav">
+        <ul id="navigation-items-mobile">
+          <li>
+            <a
+              href="/"
+              style={{ "font-family": '"Avenir Next", "Avenir", sans-serif;' }}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/about"
+              style={{ "font-family": '"Avenir Next", "Avenir", sans-serif;' }}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="/projects"
+              style={{ "font-family": '"Avenir Next", "Avenir", sans-serif;' }}
+            >
+              Contact
+            </a>
+          </li>
+          <li>
+            <a
+              href="/contact"
+              style={{ "font-family": '"Avenir Next", "Avenir", sans-serif;' }}
+            >
+              FAQ
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

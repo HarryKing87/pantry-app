f6bc8ea048c81e0dc14fae7af01f899d7367f69b
HarryKing87 <babishh96@hotmail.com>
2022-12-19 21:53:53 +0200

    Completed first phase of Home page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import Navigation from "./Navigation";
 import "./CSS/home.css";
 import "./CSS/home-banner.css";
+import "./CSS/home-button.css";
 export default function Home() {
   return (
     <div className="home-component">
@@ -8,7 +9,22 @@ export default function Home() {
       <div class="container">
         <div class="neon">Pantry. </div>
       </div>
-      <div className="home-content"></div>
+      <div className="home-content" style={{ margin: "15% 0 0 0" }}>
+        <div className="home-hero">
+          <h1 className="saveMore">Save more.</h1>
+          <h1
+            style={{
+              "border-bottom": "2.5px solid white",
+              display: "inline-block",
+            }}
+          >
+            Do less.
+          </h1>
+        </div>
+        <div className="buttonStart">
+          <button class="fade">Start</button>
+        </div>
+      </div>
     </div>
   );
 }

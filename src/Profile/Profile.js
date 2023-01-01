36b5d5078191a2655d1430196919e5e80f4aa648
HarryKing87 <babishh96@hotmail.com>
2023-01-01 18:54:57 +0200

    Fixed announcement message for profile page and paused

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -7,7 +7,6 @@ export default function Profile(props) {
   return (
     <div>
       <Navigation />
-      <h1>Profile.</h1>
       <div className="showProfileName">
         <img
           src="https://cdn3d.iconscout.com/3d/premium/thumb/profile-6073860-4996977.png"
@@ -16,6 +15,12 @@ export default function Profile(props) {
           height={"40px"}
         />
         <h4>Welcome, {loggedUser}!</h4>
+        <p>
+          <i>
+            This page is still under development and will take some time until I
+            figure out what to insert.
+          </i>
+        </p>
       </div>
     </div>
   );

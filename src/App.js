4b2fdb023353d0034e11c94f3a192e7fa7cacf9e
HarryKing87 <babishh96@hotmail.com>
2023-01-22 00:24:04 +0200

    Feature: Created dashboard preferred food and quotes along with photo carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import About from "./About";
 import Dashboard from "./Dashboard";
 import Login from "./Profile/Login";
 import Profile from "./Profile/Profile";
+import Fruits from "./Pantry/Fruits";
+import Vegetables from "./Pantry/Vegetables";
+import Dairy from "./Pantry/Dairy";
+import Meat from "./Pantry/Meat";
 import {
   BrowserRouter,
   Routes, // instead of "Switch"
@@ -28,6 +32,18 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <Routes>
+          <Route path="/fruits" element={<Fruits />} />
+        </Routes>
+        <Routes>
+          <Route path="/vegetables" element={<Vegetables />} />
+        </Routes>
+        <Routes>
+          <Route path="/dairy" element={<Dairy />} />
+        </Routes>
+        <Routes>
+          <Route path="/meat" element={<Meat />} />
         </Routes>
       </BrowserRouter>
     </div>

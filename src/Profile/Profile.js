5f8ce428a155c8f87975bc052eb02f3236984c57
HarryKing87 <babishh96@hotmail.com>
2023-01-28 12:44:59 +0200

    Created new functionality for Profile Page with Allergies

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "../Navigation";
 import "../CSS/profile.css";
 
 export default function Profile(props) {
   let loggedUser = localStorage.getItem("username");
+
+  /* Allergies Development */
+  const allergies = [
+    "Peanuts",
+    "Shellfish",
+    "Tree nuts",
+    "Eggs",
+    "Milk",
+    "Soy",
+    "Wheat",
+    "Fish",
+    "Sesame",
+  ];
+
+  const [hasAllergies, setHasAllergies] = useState(false);
+  const [selectedAllergies, setSelectedAllergies] = useState([]);
+  const [showSaveButton, setShowSaveButton] = useState(true);
+  const [setLoggedUser] = useState("");
+  const [savedAllergies, setSavedAllergies] = useState(false);
+
+  const handleAllergySelection = (e) => {
+    setHasAllergies(true);
+    setSelectedAllergies([...selectedAllergies, e.target.value]);
+  };
+
+  const handleSaveAllergies = () => {
+    localStorage.setItem(
+      `${loggedUser}_allergies`,
+      JSON.stringify(selectedAllergies)
+    );
+    setShowSaveButton(false);
+    setSavedAllergies(true);
+  };
+
+  const handleUser = (e) => {
+    setLoggedUser(e.target.value);
+  };
+
+  useEffect(() => {
+    const userAllergies = JSON.parse(
+      localStorage.getItem(`${loggedUser}_allergies`)
+    );
+    if (userAllergies) {
+      setSelectedAllergies(userAllergies);
+      setHasAllergies(true);
+      setSavedAllergies(true);
+    }
+  }, [loggedUser]);
+
   return (
     <div>
       <Navigation />
@@ -21,6 +70,51 @@ export default function Profile(props) {
             figure out what to insert.
           </i>
         </p>
+      </div>
+      <div className="allergies-table allergies-container">
+        {!savedAllergies && (
+          <>
+            <h2>Please enter your name</h2>
+            <input type="text" onChange={handleUser} />
+            <h2>Do you have any allergies?</h2>
+            <div className="checkbox-grid">
+              {allergies.map((allergy) => (
+                <label key={allergy}>
+                  <input
+                    type="checkbox"
+                    value={allergy}
+                    onChange={handleAllergySelection}
+                    checked={selectedAllergies.includes(allergy)}
+                    className="larger-checkbox"
+                  />
+                  {allergy}
+                </label>
+              ))}
+            </div>
+            {showSaveButton && (
+              <button onClick={handleSaveAllergies}>Save Allergies</button>
+            )}
+          </>
+        )}
+
+        {hasAllergies && (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th>Allergies</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedAllergies.map((allergy) => (
+                  <tr key={allergy}>
+                    <td>{allergy}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
       </div>
     </div>
   );

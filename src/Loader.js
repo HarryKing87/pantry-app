6e4d36153f136b77ce9232a660032815311c55f8
HarryKing87 <babishh96@hotmail.com>
2023-02-28 19:13:30 +0200

    Added loader and fixed minor issues

---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CSS/loader.css";
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Loader;

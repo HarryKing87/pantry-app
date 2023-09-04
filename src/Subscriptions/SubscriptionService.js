238960896d499335da685bf6000343105f2654a1
HarryKing87 <babishh96@hotmail.com>
2023-09-04 22:56:46 +0300

    Modified the correct URL for the port

--- src/Subscriptions/SubscriptionService.js
+++ src/Subscriptions/SubscriptionService.js
@@ -3,15 +3,12 @@ import React from "react";
 const SubscriptionService = () => {
   const handleCheckout = async () => {
     try {
-      const response = await fetch(
-        "http://itspantry.netlify.app:9999/create-checkout-session",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch("/.netlify/functions/server", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -19,7 +16,7 @@ const SubscriptionService = () => {
 
       const { sessionId } = await response.json();
       alert(sessionId);
-      window.location.href = `https://checkout.stripe.com/c/pay/${sessionId}#fidkdWxOYHwnPyd1blpxYHZxWjA0TG4zSDRBQnVyd0dnfWZoSTN1fTx8ZzZ9fF1WVndiPTdOQ0tPNmtWM3NVf38za1NiTkA3YlJqN2ZRSGszVEpCfXRWbkN0dEldNWRqUl1BV3F8QTZ0S2d3NTUyUGJqN200aicpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPyd2bGtiaWBabHFgaCcpJ2BrZGdpYFVpZGZgbWppYWB3dic%2FcXdwYHgl`;
+      window.location.href = `https://checkout.stripe.com/c/pay/${sessionId}#fidkdWxOYHwnPyd1blpxYHZxWjA0TG4zSDRBQnVyd0dnfWZoSTN1fTx8ZzZ9fF1WVndiPTdOQ0tPNmtWM3NVf38za1NiTkA3YlJqN2ZRSGszVEpCfXRWbkN0dEldNWRqUl1BV3F8QTZ0KzczN0NiYXpXaVFzU2xlQWRsNGdPVXBtcyZvaG1mbDk2cXR0d2FgbGhlVw%3D%3D`;
     } catch (error) {
       console.error("Error initiating checkout:", error);
     }

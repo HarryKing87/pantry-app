b7b6aee65149c124aa40d3b3271f6c5074f5d182
HarryKing87 <babishh96@hotmail.com>
2023-09-09 10:18:21 +0300

    Added subscription information next to user full name

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(
+/*const stripe = require("stripe")(
   "sk_test_51Ik6M1DGpwrBbxcmOs6t6tHPgHAzKSjowSDY0ZWfrIIW4zzMPkqLwJ0eT7Dn7Ym0bSmakJP63j91IaErsc2ntNsg00FmjGvBEK"
 );
 
@@ -29,3 +29,39 @@ exports.handler = async (event, context) => {
     };
   }
 };
+*/
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Ik6M1DGpwrBbxcmOs6t6tHPgHAzKSjowSDY0ZWfrIIW4zzMPkqLwJ0eT7Dn7Ym0bSmakJP63j91IaErsc2ntNsg00FmjGvBEK"
+);
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: "http://localhost:3000",
+      cancel_url: "http://localhost:3000/profile",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1NjS6vDGpwrBbxcmyjQepCM7",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+    });
+
+    res.json({ sessionId: session.id });
+  } catch (error) {
+    console.error("Error creating session:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});

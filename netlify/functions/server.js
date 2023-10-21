3b93eaf031a06dc77264ceb92ae20540b0cb3ca1
HarryKing87 <babishh96@hotmail.com>
2023-10-21 15:42:47 +0300

    Reversed general code to server

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,66 +2,30 @@ const stripe = require("stripe")(
   "sk_test_51Ik6M1DGpwrBbxcmOs6t6tHPgHAzKSjowSDY0ZWfrIIW4zzMPkqLwJ0eT7Dn7Ym0bSmakJP63j91IaErsc2ntNsg00FmjGvBEK"
 );
 
-exports.createSubscription = async (event, context) => {
-  if (event.httpMethod === "GET") {
-    try {
-      const session = await stripe.checkout.sessions.create({
-        success_url: "https://itspantry.netlify.app",
-        cancel_url: "http://localhost:3000/profile",
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price: "price_1NjS6vDGpwrBbxcmyjQepCM7",
-            quantity: 1,
-          },
-        ],
-        mode: "subscription",
-      });
+exports.handler = async (event, context) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: "https://itspantry.netlify.app",
+      cancel_url: "http://localhost:3000/profile",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1NjS6vDGpwrBbxcmyjQepCM7",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+    });
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ sessionId: session.id }),
-      };
-    } catch (error) {
-      console.error("Error creating session:", error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: "An error occurred" }),
-      };
-    }
-  } else {
     return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Invalid request" }),
+      statusCode: 200,
+      body: JSON.stringify({ sessionId: session.id }),
     };
-  }
-};
-
-exports.cancelSubscription = async (event, context) => {
-  if (event.httpMethod === "POST") {
-    try {
-      const { subscriptionId } = JSON.parse(event.body);
-
-      // Cancel the subscription
-      const canceledSubscription = await stripe.subscriptions.del(
-        subscriptionId
-      );
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Subscription canceled" }),
-      };
-    } catch (error) {
-      console.error("Error canceling subscription:", error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: "An error occurred" }),
-      };
-    }
-  } else {
+  } catch (error) {
+    console.error("Error creating session:", error);
     return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Invalid request" }),
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred" }),
     };
   }
 };

81f5d71cc4b5764024612444bad25921d1b0b985
Coding_Harry <babishh96@hotmail.com>
2023-12-02 19:31:40 +0200

    Cleaned file structure

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
-import Navigation from "./Navigation";
+import Navigation from "../Navigation";
 import { useState, useEffect } from "react";
-import Dashboard from "./Dashboard-Premium";
+import ImageScrollerPremium from "./Dashboard-Premium";
 import {
   getFirestore,
   collection,
@@ -8,23 +8,15 @@ import {
   where,
   getDocs,
   setDoc,
-  updateDoc,
   doc,
 } from "firebase/firestore";
-import { auth } from "./Database/firebase";
+import { auth } from "../Database/firebase";
 import { useNavigate } from "react-router-dom";
-import "./CSS/dashboard.css";
+import "../CSS/dashboard.css";
 
 const db = getFirestore();
 
-const ImageScroller = ({
-  image1 = "https://images.unsplash.com/photo-1546548970-71785318a17b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZnJ1aXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-  image2 = "https://images.unsplash.com/photo-1590779033100-9f60a05a013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
-  image3 = "https://images.unsplash.com/photo-1529258283598-8d6fe60b27f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGRhaXJ5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-  image4 = "https://images.unsplash.com/photo-1603048297172-c92544798d5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1lYXR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
-  image5 = "https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFzdGF8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
-  image6 = "https://images.unsplash.com/photo-1586585571612-bbc4176c9bf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFudHJ5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-}) => {
+const ImageScroller = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [allergies, setAllergies] = useState({
@@ -87,7 +79,7 @@ const ImageScroller = ({
     <>
       <Navigation />
 
-      {isUserPremium ? <Dashboard /> : <h1>You are not a premium user</h1>}
+      {isUserPremium ? <ImageScrollerPremium /> : <h1>You are not a premium user</h1>}
     </>
   );
 };

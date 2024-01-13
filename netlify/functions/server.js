881614ab65e144b353802a06797e6f84e175924b
Coding_Harry <babishh96@hotmail.com>
2024-01-13 12:50:27 +0200

    Populated server js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,20 +1,70 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// TEST MODE
+const stripe = require("stripe")(
+  "sk_test_51Ik6M1DGpwrBbxcmOs6t6tHPgHAzKSjowSDY0ZWfrIIW4zzMPkqLwJ0eT7Dn7Ym0bSmakJP63j91IaErsc2ntNsg00FmjGvBEK"
+);
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBjpiNp1Po5Ps6iDxuscA-ulvVtr3MfASQ",
-  authDomain: "pantry-app-c3916.firebaseapp.com",
-  projectId: "pantry-app-c3916",
-  storageBucket: "pantry-app-c3916.appspot.com",
-  messagingSenderId: "886634612358",
-  appId: "1:886634612358:web:5698c428ae39d25aa8f307",
+exports.handler = async (event, context) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: "https://itspantry.netlify.app",
+      cancel_url: "http://localhost:3000/profile",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1NjS6vDGpwrBbxcmyjQepCM7",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ sessionId: session.id }),
+    };
+  } catch (error) {
+    console.error("Error creating session:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred" }),
+    };
+  }
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+/* TESTING FLOW
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Ik6M1DGpwrBbxcmOs6t6tHPgHAzKSjowSDY0ZWfrIIW4zzMPkqLwJ0eT7Dn7Ym0bSmakJP63j91IaErsc2ntNsg00FmjGvBEK"
+);
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: "http://localhost:3000",
+      cancel_url: "http://localhost:3000/profile",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1NjS6vDGpwrBbxcmyjQepCM7",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+    });
+
+    res.json({ sessionId: session.id });
+  } catch (error) {
+    console.error("Error creating session:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+*/

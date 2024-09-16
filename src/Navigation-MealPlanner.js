f46bf91b7a6dd54a2820b1d0466d1449945a4093
ckynigopoulos <babishh96@hotmail.com>
2024-09-16 21:37:34 +0300

    Information icon development

--- src/Navigation-MealPlanner.js
+++ src/Navigation-MealPlanner.js
@@ -3,9 +3,14 @@ import { useState, useEffect } from "react";
 /* Font Awesome icons */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { faQuestion, faBell } from "@fortawesome/free-solid-svg-icons";
+import {
+  faQuestion,
+  faBell,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 // Icon set for profile pic
 import { Avatar } from "primereact/avatar";
+import { Card } from "primereact/card";
 import {
   getFirestore,
   collection,
@@ -13,42 +18,45 @@ import {
   where,
   getDocs,
   setDoc,
-  updateDoc,
   doc,
 } from "firebase/firestore";
-import { signOut } from "firebase/auth";
+import { Dialog } from "primereact/dialog";
 import { auth } from "./Database/firebase";
 
 const db = getFirestore();
 
 export default function NavigationMealPlan() {
   const [user, setUser] = useState(null);
+  const [infoVisible, setInfoVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-        const userRef = doc(db, "users", user.uid);
-        const q = query(collection(db, "users"), where("id", "==", user.uid));
-        getDocs(q)
-          .then((querySnapshot) => {
-            if (querySnapshot.empty) {
-              setDoc(userRef, {
-                id: user.uid,
-                selectedImage,
-              });
-            } else {
-              const data = querySnapshot.docs[0].data();
-              setSelectedImage(data.userImage);
-            }
-          })
-          .catch((error) => {
-            console.error("Error getting user data:", error);
-          });
-      } else {
-        setUser(null);
-      }
-    });
+    const unsubscribe = auth.onAuthStateChanged(
+      (user) => {
+        if (user) {
+          setUser(user);
+          const userRef = doc(db, "users", user.uid);
+          const q = query(collection(db, "users"), where("id", "==", user.uid));
+          getDocs(q)
+            .then((querySnapshot) => {
+              if (querySnapshot.empty) {
+                setDoc(userRef, {
+                  id: user.uid,
+                  selectedImage,
+                });
+              } else {
+                const data = querySnapshot.docs[0].data();
+                setSelectedImage(data.userImage);
+              }
+            })
+            .catch((error) => {
+              console.error("Error getting user data:", error);
+            });
+        } else {
+          setUser(null);
+        }
+      },
+      [user]
+    );
     return () => {
       unsubscribe();
     };
@@ -73,10 +81,15 @@ export default function NavigationMealPlan() {
         </div>
         <ul className="mealplanner-ul">
           <li>
-            <FontAwesomeIcon icon={faQuestion} className="mealplanner-icon" />
+            <FontAwesomeIcon
+              icon={faQuestion}
+              className="mealplanner-icon"
+              onClick={() => setInfoVisible(true)}
+            />
           </li>
-          <li>
+          <li className="notification-container">
             <FontAwesomeIcon icon={faBell} className="mealplanner-icon" />
+            {/* <span className="badge">5</span> */}
           </li>
           <li>
             <a href="/Profile">
@@ -90,6 +103,90 @@ export default function NavigationMealPlan() {
           </li>
         </ul>
       </nav>
+      <Dialog
+        className="info-Dialog"
+        header="Seeking Information?"
+        visible={infoVisible}
+        onHide={() => setInfoVisible(false)}
+      >
+        <div className="info-panel">
+          <div className="info-cards">
+            <Card
+              title="🍎 Your Healthy Choice"
+              subTitle="Fresh & Nutritious"
+              style={{ width: "300px", marginBottom: "20px" }}
+              className="info-individualCard"
+            >
+              <p>
+                Enjoy a world of nutritious and delicious meal options right at
+                your fingertips. Our meal planner helps you stay on track with
+                fresh, healthy choices tailored to your needs.
+              </p>
+            </Card>
+
+            <Card
+              title="📅 Plan Your Week"
+              subTitle="Organize Effortlessly"
+              style={{ width: "300px", marginBottom: "20px" }}
+              className="info-individualCard"
+            >
+              <p>
+                Keep your week organized with our easy-to-use planner. Schedule
+                your meals for each day and never worry about last-minute
+                decisions.
+              </p>
+            </Card>
+
+            <Card
+              title="📋 Keep Track"
+              subTitle="Notes & Tags"
+              style={{ width: "300px", marginBottom: "20px" }}
+              className="info-individualCard"
+            >
+              <p>
+                Add notes and tags to each meal to track your preferences and
+                make adjustments as needed. Our planner makes it easy to
+                remember what works best for you.
+              </p>
+            </Card>
+
+            <Card
+              title="🍽️ Meal Prep Made Simple"
+              subTitle="Efficient & Easy"
+              style={{ width: "300px", marginBottom: "20px" }}
+              className="info-individualCard"
+            >
+              <p>
+                Streamline your meal prep with our intuitive interface. Quickly
+                add, edit, and manage meals to ensure you’re always ready for a
+                healthy, satisfying meal.
+              </p>
+            </Card>
+          </div>
+          <div
+            style={{
+              margin: "0 auto",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <p>Would you like to communicate or have any ideas?</p>
+            <a
+              href="mailto:harrykinghsv@gmail.com"
+              className="email-button"
+              style={{ margin: "0 auto" }}
+            >
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                style={{ marginRight: "5px" }}
+              />{" "}
+              Send Us a Message
+            </a>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 }

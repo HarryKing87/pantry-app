43bac8058d171d05243549e6db8c4bafa861a073
ckynigopoulos <babishh96@hotmail.com>
2024-09-22 18:23:57 +0300

    Fixed 0 values for meal plan food

--- src/Profile/MealPlanFoodInfo.jsx
+++ src/Profile/MealPlanFoodInfo.jsx
@@ -4,14 +4,21 @@ export default function MealPlanFoodInfo({ item, nutriData }) {
   return (
     <div>
       <div className="foodInfo-general">
-        <p>
-          Want to dive deeper into the food choices in your meal planner? Unlock
-          a new level of understanding about what you consume, with detailed
-          insights into the nutrients of every item in your plan. Pantry
-          captures all this data seamlessly, just for you. Take your time, plan
-          with precision, and schedule effortlessly. We're thrilled to have you
-          here!
-        </p>
+        {item.notes ? (
+          <div>
+            <h3>Notes</h3>
+            <p>{item.notes}</p>
+          </div>
+        ) : (
+          <p>
+            Want to dive deeper into the food choices in your meal planner?
+            Unlock a new level of understanding about what you consume, with
+            detailed insights into the nutrients of every item in your plan.
+            Pantry captures all this data seamlessly, just for you. Take your
+            time, plan with precision, and schedule effortlessly. We're thrilled
+            to have you here!
+          </p>
+        )}
       </div>
 
       <div>
@@ -26,12 +33,19 @@ export default function MealPlanFoodInfo({ item, nutriData }) {
             </thead>
             <tbody>
               {Object.entries(nutriData[item.foodName] || {}).map(
-                ([nutrient, value]) => (
-                  <tr key={nutrient}>
-                    <td>{nutrient}</td>
-                    <td>{value}</td>
-                  </tr>
-                )
+                ([nutrient, value]) => {
+                  // Removing the value that has 0 as the value
+                  if (value === 0) {
+                    return null;
+                  } else {
+                    return (
+                      <tr key={nutrient}>
+                        <td>{nutrient}</td>
+                        <td>{value}</td>
+                      </tr>
+                    );
+                  }
+                }
               )}
             </tbody>
           </table>
